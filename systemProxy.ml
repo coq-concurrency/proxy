25085731b7755398d8f90153dd1d9d2eb2f7314f
Guillaume Claret <dev@clarus.me>
2014-09-23 18:42:02 +0200

    Beginning of File.read

--- systemProxy.ml
+++ systemProxy.ml
@@ -5,12 +5,23 @@ module Log = struct
     | _ -> failwith "wrong number of arguments"
 end
 
+module File = struct
+  let read (arguments : string list) : unit Lwt.t =
+    match arguments with
+    | [file_name] ->
+      let lines = Lwt_io.lines_of_file file_name in
+      Lwt.bind (Lwt_stream.fold (fun s1 s2 -> s1 ^ "\n" ^ s2) lines "") (fun content ->
+      Lwt_io.print content)
+    | _ -> failwith "wrong number of arguments"
+end
+
 let handle (message : string) : unit Lwt.t =
   match Str.split (Str.regexp_string " ") message with
   | [] -> failwith "message empty"
   | command :: arguments ->
     match command with
     | "Log.write" -> Log.write arguments
+    | "File.read" -> File.read arguments
     | _ -> failwith "unknown command"
 
 let rec main () : unit Lwt.t =
