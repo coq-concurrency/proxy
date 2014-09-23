9cb36a29db5a7f77d4b85c7b073f25b271e56646
Guillaume Claret <dev@clarus.me>
2014-09-23 18:04:46 +0200

    Simple message handler

--- systemProxy.ml
+++ systemProxy.ml
@@ -1,4 +1,8 @@
-let main () : unit Lwt.t =
-  Lwt_io.printl "Hello world!"
+let handle (message : string) : unit Lwt.t =
+  Lwt_io.printl message
+
+let rec main () : unit Lwt.t =
+  Lwt.bind (Lwt_io.read_line Lwt_io.stdin) (fun message ->
+  Lwt.join [handle message; main ()])
 
 ;;Lwt_main.run (main ())
